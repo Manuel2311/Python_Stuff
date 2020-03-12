b145bd0280060920d0dbea4c8418f0cda8986e50
Manuel2311 <manuel.gar.san@techtalents.club>
2020-03-12 17:37:24 +0100

    I modified the binary search to make it a more accesible and functional file

--- binary_search.py
+++ binary_search.py
@@ -1,19 +1,22 @@
+from typing import Any, Optional, Callable, Union, Text
+
 def binary_search(data,needle,key=None):
     left_index=0
-    right_index=len(numbers)
+    right_index=len(data)
 
     while right_index - left_index>1:
         middle_index=(right_index-left_index) // 2 + left_index
         print(middle_index)
         if key:
             datum=key(data[middle_index])
+        else:
+            datum=data[middle_index]
         if datum>needle:
             right_index=middle_index
         elif datum<needle:
             left_index=middle_index
         else:
-            return middle_index
-
+            return middle_index + 1     
 
 def main(needle):
     with open("Data/data_nums_1000000.txt","r") as file:
@@ -23,3 +26,7 @@ def main(needle):
             for n in numbers:
                 file2.write(f"{n}\n")
 
+assert binary_search([4,8,15,16,23,42],15)==2,"OMG THE WORLD IS ON FIRE"
+
+if __name__ == "__main__":
+    pass
